9868b4dc4ec50349044c6d7d86411196f38b41ee
Innocenti Davide <d.innocenti@chemitec.it>
2025-05-26 15:58:31 +0200

    Inizio modifiche FW, introdotta gestione cartelle

--- FW_ArduFaba/FW_ArduFaba.ino
+++ FW_ArduFaba/FW_ArduFaba.ino
@@ -1,6 +1,8 @@
 #include <SPI.h>
 #include <MFRC522.h>
 #include "SerialMP3Player.h"
+#include "Button2.h"
+#include "EEPROM.h"
 
 // Pin MFRC522
 #define RST_PIN  9
@@ -9,6 +11,11 @@
 // Pin MP3 Player YX5300 (TX modulo → pin 4, RX modulo → pin 5)
 #define MP3_TX 4
 #define MP3_RX 5
+
+const int MAX_TAGS = 100;
+const int TAG_SIZE = 4;
+const int COUNT_ADDR = 0;
+const int DATA_START_ADDR = 1;
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 SerialMP3Player mp3(MP3_RX, MP3_TX);
@@ -19,6 +26,8 @@ byte tag2_UID[4] = {0x12, 0x34, 0x56, 0x78}; // Brano 2
 
 bool isPlaying = false;
 int currentTrack = 0;
+
+
 
 void setup() {
   Serial.begin(9600);
@@ -32,7 +41,11 @@ void setup() {
   mp3.sendCommand(CMD_SEL_DEV, 0, 2); // seleziona SD card
   delay(500);
 
-  mp3.setVol(15); // Volume 50%
+  playFileInFolder(1, 1);
+
+  delay(5000);
+
+  mp3.setVol(30); // Volume 50%
 
   Serial.println("Sistema pronto. Avvicina un tag RFID.");
 }
@@ -65,6 +78,7 @@ void loop() {
         }
       }
     }
+
   }
 
   // Normale lettura tag RFID per cambiare brano
@@ -103,4 +117,40 @@ bool compareUID(byte *uid1, byte *uid2) {
     if (uid1[i] != uid2[i]) return false;
   }
   return true;
+}
+
+void playFileInFolder(uint8_t folder, uint8_t file) {
+  
+  if (mp3.available()) 
+  {
+    mp3.sendCommand(0x0F, folder, file);
+  }
+}
+
+int findTag(byte* uid) {
+  int count = EEPROM.read(COUNT_ADDR);
+  for (int i = 0; i < count; i++) {
+    int base = DATA_START_ADDR + i * TAG_SIZE;
+    bool match = true;
+    for (int j = 0; j < TAG_SIZE; j++) {
+      if (EEPROM.read(base + j) != uid[j]) {
+        match = false;
+        break;
+      }
+    }
+    if (match) return i + 1; // mappa da 1 a 100
+  }
+  return 0; // non trovato
+}
+
+int registerNewTag(byte* uid) {
+  int count = EEPROM.read(COUNT_ADDR);
+  if (count >= MAX_TAGS) return -1; // memoria piena
+
+  int base = DATA_START_ADDR + count * TAG_SIZE;
+  for (int j = 0; j < TAG_SIZE; j++) {
+    EEPROM.write(base + j, uid[j]);
+  }
+  EEPROM.write(COUNT_ADDR, count + 1);
+  return count + 1; // ID assegnato da 1 a 100
 }
