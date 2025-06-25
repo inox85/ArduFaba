a940d25b331b6b87e54621a559485e108f682ab0
Innocenti Davide <d.innocenti@chemitec.it>
2025-06-25 17:09:01 +0200

    Ulteriori modifiche

--- FW_ArduFaba/FW_ArduFaba.ino
+++ FW_ArduFaba/FW_ArduFaba.ino
@@ -3,7 +3,7 @@
 #include "SerialMP3Player.h"
 #include "Button2.h"
 #include "EEPROM.h"
-
+#include <SoftwareSerial.h>
 
 #define GREEN_BUTTON_PIN 7
 #define RED_BUTTON_PIN 8
@@ -35,43 +35,57 @@ const int COUNT_ADDR = 0;
 const int DATA_START_ADDR = 1;
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
+SoftwareSerial mp3Serial(MP3_RX, MP3_TX);  // RX, TX del modulo MP3
 SerialMP3Player mp3(MP3_RX, MP3_TX);
-
 
 bool isPlaying = false;
 int currentTrack = 0;
 
-Button2 redButton(GREEN_BUTTON_PIN);
-Button2 greenButton(RED_BUTTON_PIN);
+Button2 redButton;
+Button2 greenButton;
 
-// Funzione richiamata al singolo click
-void redButtonSingleClick(Button2& btn) {
-  Serial.println("Click singolo!");
+void redButtonHandler(Button2& btn) {
+    switch (btn.getType()) {
+        case single_click:
+            break;
+        case double_click:
+            Serial.print("double ");
+            break;
+        case triple_click:
+            Serial.print("triple ");
+            break;
+        case long_click:
+            Serial.print("long");
+            break;
+          case empty:
+            return;
+    }
+    Serial.print("click");
+    Serial.print(" (");
+    Serial.print(btn.getNumberOfClicks());    
+    Serial.println(")");
 }
 
-// Funzione richiamata al doppio click
-void redButtonDoubleClick(Button2& btn) {
-  Serial.println("Doppio click!");
-}
-
-// Funzione richiamata alla pressione lunga
-void redButtonLongClick(Button2& btn) {
-  Serial.println("Pressione lunga!");
-}
-
-void greenButtonSingleClick
-(Button2& btn) {
-  Serial.println("Click singolo!");
-}
-
-// Funzione richiamata al doppio click
-void greenButtonDoubleClick(Button2& btn) {
-  Serial.println("Doppio click!");
-}
-
-// Funzione richiamata alla pressione lunga
-void greenButtonLongClick(Button2& btn) {
-  Serial.println("Pressione lunga!");
+void greenButtonHandler(Button2& btn) {
+    switch (btn.getType()) {
+        case single_click:
+            break;
+        case double_click:
+            Serial.print("double ");
+            break;
+        case triple_click:
+            Serial.print("triple ");
+            break;
+        case long_click:
+            Serial.print("long");
+            break;
+          case empty:
+            return;
+    }
+    Serial.print("click");
+    Serial.print(" (");
+    Serial.print(btn.getNumberOfClicks());    
+    Serial.println(")");
 }
 
 void setup() 
@@ -99,13 +113,19 @@ void setup()
 
   Serial.println("Inizializzazione Bottoni...");
   // Definiamo i gestori degli eventi
-  redButton.setClickHandler(redButtonSingleClick);
-  redButton.setDoubleClickHandler(redButtonDoubleClick);
-  redButton.setLongClickHandler(redButtonLongClick);
+  redButton.begin(RED_BUTTON_PIN);
+  redButton.setClickHandler(redButtonHandler);
+  //button.setLongClickHandler(handler);       // this will only be called upon release
+  redButton.setLongClickDetectedHandler(redButtonHandler);  // this will only be called upon detection
+  redButton.setDoubleClickHandler(redButtonHandler);
+  redButton.setTripleClickHandler(redButtonHandler);
 
-  greenButton.setClickHandler(greenButtonSingleClick);
-  greenButton.setDoubleClickHandler(greenButtonDoubleClick);
-  greenButton.setLongClickHandler(greenButtonLongClick);
+  greenButton.begin(GREEN_BUTTON_PIN);
+  greenButton.setClickHandler(greenButtonHandler);
+  //button.setLongClickHandler(handler);       // this will only be called upon release
+  greenButton.setLongClickDetectedHandler(greenButtonHandler);  // this will only be called upon detection
+  greenButton.setDoubleClickHandler(greenButtonHandler);
+  greenButton.setTripleClickHandler(greenButtonHandler);
 
   
   playFileInFolder(MISC_FOLDER, POWER_UP_TRACK);
@@ -127,8 +147,12 @@ void eraseEEPROM()
   Serial.println("EEPROM cancellata.");
 }
 
-void loop() {
+void loop() 
+{
 
+  redButton.loop();
+  greenButton.loop();
+  
   if(mp3.available())
   {
     // Normale lettura tag RFID per cambiare brano
@@ -196,14 +220,23 @@ void loop() {
 
 }
 
+
 void playFileInFolder(uint8_t folder, uint8_t file) 
 { 
   if (mp3.available()) 
   {
     mp3.sendCommand(0x0F, folder, file);
+    Serial.print("Inizio messaggio -> ");
+    Serial.print("Cartella: ");
+    Serial.print(folder);
+    Serial.print(" File: ");
+    Serial.println(file);
     delay(5000);
+    Serial.print("Fine messaggio");
   }
 }
+
+
 
 
 int findTag(byte* uid) {
